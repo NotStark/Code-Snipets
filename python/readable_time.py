f65d444b80c29ae9f078c375d6dac8a14647ab47
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-05-04 06:55:58 +0530

    Update readable_time.py

--- python/readable_time.py
+++ python/readable_time.py
@@ -1,18 +1,19 @@
 
-
-def readable_time(seconds : int) -> str:
-    time = seconds
+def readable_time(seconds: int) -> str:
     time_string = ""
-    if time < 60:
-        time_string = f"{time}s"
+    if seconds < 0:
+        raise ValueError("Input value must be non-negative")
+
+    if seconds < 60:
+        time_string = f"{round(seconds)}s"
     else:
-        minutes, seconds = divmod(time, 60)
+        minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
         days, hours = divmod(hours, 24)
         if days > 0:
-            time_string += f"{days}days, "
+            time_string += f"{round(days)}days, "
         if hours > 0:
-            time_string += f"{hours}h:"
-        time_string += f"{minutes}m:{seconds}s"
+            time_string += f"{round(hours)}h:"
+        time_string += f"{round(minutes)}m:{round(seconds)}s"
 
     return time_string
