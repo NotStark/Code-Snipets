08c8648cbf88ba7a67b123e9e24c66b20fe89479
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-05-11 14:01:07 +0530

    Create update_repo.py

---
+++ python/update_repo.py
@@ -0,0 +1,9 @@
+import subprocess
+
+def update_repo(path : str, git_token : str, git_username : str, repo_name : str):
+    try : 
+        subprocess.check_output(f'cd {path}', shell=True)
+        subprocess.check_output(f'git pull https://{git_token}@github.com/{git_username}/{repo_name}.git', shell=True)
+        print("Sucess! Updated the Repo")
+    except Exception as e:
+        print(e)
